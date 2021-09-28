0ca06eba5681164d755c622d7110c8490f4b1856
arjun <arjun2000raj@gmail.com>
2021-09-29 02:28:35 +0530

    style - add css to tha app

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const calculateProfitLoss = (initial, current, quantity) => {
 
         const loss = (initial - current) * quantity;
         const lossPercentage = (loss / initial) * 100;
-        output.style.color = "red";
+        output.style.color = "blue";
         output.innerText = `Your loss is ${loss.toFixed(2)} and loss percentage is ${lossPercentage.toFixed(2)} %`;
 
     } else if (current > initial) {
 
         const profit = (current - initial) * quantity;
         const profitPercentage = (profit / initial) * 100;
-        output.style.color = "gree";
+        output.style.color = "green";
         output.innerText = `Your profit is ${profit.toFixed(2)} and profit percentage is ${profitPercentage.toFixed(2)} %`;
     } else {
-        output.style.color = "white";
+        output.style.color = "black";
         output.innerText = 'No pain no gain no gain no pain';
     }
 }
@@ -27,7 +27,10 @@ const clickHandler = () => {
     const ip = Number(initialPrice.value);
     const qty = Number(stockQuantity.value);
     const curr = Number(currentPrice.value);
-
+    if(ip < 0 || qty < 0 || curr < 0){
+        output.style.color = "red";
+        output.innerText = 'Input can\'t be negaive.';
+    }else
     calculateProfitLoss(ip,curr,qty);
 }
 tellmeBtn.addEventListener('click', clickHandler)
